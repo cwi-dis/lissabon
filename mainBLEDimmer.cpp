522d6669d44f8fa22ea2f95cb7ad0b2da605fbe5
Jack Jansen <Jack.Jansen@cwi.nl>
2020-04-30 23:35:22 +0200

    Use UpDownButtons

--- mainBLEDimmer.cpp
+++ mainBLEDimmer.cpp
@@ -31,16 +31,20 @@ IotsaOtaMod otaMod(application);
 IotsaBatteryMod batteryMod(application);
 
 #include "iotsaInput.h"
-Touchpad touchpad12(12, true, false, true);
-Touchpad touchpad13(13, true, false, true);
-Touchpad touchpad14(14, true, false, true);
-Touchpad touchpad15(15, true, false, true);
+Touchpad touch1down(12, true, true, true);
+Touchpad touch1up(13, true, true, true);
+UpDownButtons encoder1(touch1down, touch1up, true);
+#ifdef WITH_SECOND_DIMMER
+Touchpad touch2down(14, true, false, true);
+Touchpad touch2up(15, true, false, true);
+UpDownButtons encoder2(touch2down, touch2up, true);
+#endif
 
 Input* inputs[] = {
-  &touchpad12,
-  &touchpad13,
-  &touchpad14,
-  &touchpad15
+  &encoder1,
+#ifdef WITH_SECOND_DIMMER
+  &encoder2,
+#endif
 };
 
 IotsaInputMod touchMod(application, inputs, sizeof(inputs)/sizeof(inputs[0]));
@@ -55,7 +59,7 @@ BLEUUID ledstripServiceUUID("153C0001-D28E-40B8-84EB-7F64B56D4E2E");
 // LED Lighting control module. 
 //
 
-class iotsaBLEDimmerMod : public IotsaApiMod {
+class IotsaBLEDimmerMod : public IotsaApiMod {
 public:
   using IotsaApiMod::IotsaApiMod;
   void setup();
@@ -71,90 +75,151 @@ protected:
   void deviceFound(BLEAdvertisedDevice& device);
 private:
   void handler();
-  bool touch12();
-  bool touch13();
-  bool touch14();
-  bool touch15();
+  bool touched1OnOff();
+  bool level1Changed();
+  void updateDimmer1();
+  bool isOn1;
+  float level1;
+  float minLevel1;
+#ifdef WITH_SECOND_DIMMER
+  bool touched2OnOff();
+  bool level2Changed();
+  void updateDimmer2();
+  bool isOn2;
+  float level2;
+  float minLevel2;
+#endif
+  uint32_t saveAtMillis = 0;
 };
 
 bool
-iotsaBLEDimmerMod::touch12() {
-  IFDEBUG IotsaSerial.println("touch12()");
+IotsaBLEDimmerMod::touched1OnOff() {
+  IFDEBUG IotsaSerial.printf("touched1OnOff: onOff=%d level=%f\n", isOn1, level1);
   return true;
 }
 
 bool
-iotsaBLEDimmerMod::touch13() {
-  IFDEBUG IotsaSerial.println("touch13()");
+IotsaBLEDimmerMod::level1Changed() {
+  IFDEBUG IotsaSerial.printf("level1Changed: onOff=%d level=%f\n", isOn1, level1);
+  return true;
+}
+
+void IotsaBLEDimmerMod::updateDimmer1() {
+  // xxxjack send onOff and level to dimmer
+  iotsaConfig.postponeSleep(2100);
+  saveAtMillis = millis() + 2000;
+}
+
+#ifdef WITH_SECOND_DIMMER
+bool
+IotsaBLEDimmerMod::touched2OnOff() {
+  IFDEBUG IotsaSerial.printf("touched2OnOff: onOff=%d level=%f\n", isOn2, level2);
   return true;
 }
 
 bool
-iotsaBLEDimmerMod::touch14() {
-  IFDEBUG IotsaSerial.println("touch14()");
+IotsaBLEDimmerMod::level2Changed() {
+  IFDEBUG IotsaSerial.printf("level2Changed: onOff=%d level=%f\n", isOn2, level2);
   return true;
 }
 
-bool
-iotsaBLEDimmerMod::touch15() {
-  IFDEBUG IotsaSerial.println("touch15()");
-  return true;
+void IotsaBLEDimmerMod::updateDimmer2() {
+  // xxxjack send onOff and level to dimmer
+  iotsaConfig.postponeSleep(2100);
+  saveAtMillis = millis() + 2000;
 }
+#endif // WITH_SECOND_DIMMER
 
 #ifdef IOTSA_WITH_WEB
 void
-iotsaBLEDimmerMod::handler() {
+IotsaBLEDimmerMod::handler() {
 }
 
-String iotsaBLEDimmerMod::info() {
+String IotsaBLEDimmerMod::info() {
   return "";
 }
 #endif // IOTSA_WITH_WEB
 
-bool iotsaBLEDimmerMod::getHandler(const char *path, JsonObject& reply) {
+bool IotsaBLEDimmerMod::getHandler(const char *path, JsonObject& reply) {
   return true;
 }
 
-bool iotsaBLEDimmerMod::putHandler(const char *path, const JsonVariant& request, JsonObject& reply) {
+bool IotsaBLEDimmerMod::putHandler(const char *path, const JsonVariant& request, JsonObject& reply) {
   return true;
 }
 
-void iotsaBLEDimmerMod::serverSetup() {
+void IotsaBLEDimmerMod::serverSetup() {
 }
 
 
-void iotsaBLEDimmerMod::configLoad() {
+void IotsaBLEDimmerMod::configLoad() {
+  IotsaConfigFileLoad cf("/config/bledimmer.cfg");
+  int value;
+  cf.get("isOn1", value, 0);
+  isOn1 = value;
+  cf.get("level1", level1, 0.0);
+  cf.get("minLevel1", minLevel1, 0.1);
+#ifdef WITH_SECOND_DIMMER
+  cf.get("isOn2", value, 0);
+  isOn2 = value;
+  cf.get("level2", level2, 0.0);
+  cf.get("minLevel2", minLevel2, 0.1);
+#endif // WITH_SECOND_DIMMER
 }
 
-void iotsaBLEDimmerMod::configSave() {
+void IotsaBLEDimmerMod::configSave() {
+  IotsaConfigFileSave cf("/config/bledimmer.cfg");
+
+  cf.put("level1", level1);
+  cf.put("isOn1", isOn1);
+  cf.put("minLevel1", minLevel1);
+#ifdef WITH_SECOND_DIMMER
+  cf.put("level2", level2);
+  cf.put("isOn2", isOn2);
+  cf.put("minLevel2", minLevel2);
+#endif // WITH_SECOND_DIMMER
 }
 
-void iotsaBLEDimmerMod::setup() {
+void IotsaBLEDimmerMod::setup() {
 #ifdef PIN_DISABLESLEEP
   batteryMod.setPinDisableSleep(PIN_DISABLESLEEP);
 #endif
-  touchpad12.setCallback(std::bind(&iotsaBLEDimmerMod::touch12, this));
-  touchpad13.setCallback(std::bind(&iotsaBLEDimmerMod::touch13, this));
-  touchpad14.setCallback(std::bind(&iotsaBLEDimmerMod::touch14, this));
-  touchpad15.setCallback(std::bind(&iotsaBLEDimmerMod::touch15, this));
-  auto callback = std::bind(&iotsaBLEDimmerMod::deviceFound, this, std::placeholders::_1);
+  encoder1.setCallback(std::bind(&IotsaBLEDimmerMod::level1Changed, this));
+  // Bind up/down buttons to variable illum, ranging from minLevel to 1.0 in 25 steps
+  encoder1.bindVar(level1, minLevel1, 1.0, 0.02);
+  encoder1.bindStateVar(isOn1);
+  encoder1.setStateCallback(std::bind(&IotsaBLEDimmerMod::touched1OnOff, this));
+#ifdef WITH_SECOND_DIMMER
+  encoder2.setCallback(std::bind(&IotsaBLEDimmerMod::level2Changed, this));
+  // Bind up/down buttons to variable illum, ranging from minLevel to 1.0 in 25 steps
+  encoder2.bindVar(level2, minLevel2, 1.0, 0.02);
+  encoder2.bindStateVar(isOn2);
+  encoder2.setStateCallback(std::bind(&IotsaBLEDimmerMod::touched2OnOff, this));
+#endif // WITH_SECOND_DIMMER
+
+  auto callback = std::bind(&IotsaBLEDimmerMod::deviceFound, this, std::placeholders::_1);
   bleClientMod.setDeviceFoundCallback(callback);
   bleClientMod.setServiceFilter(ledstripServiceUUID);
 }
 
-void iotsaBLEDimmerMod::deviceFound(BLEAdvertisedDevice& device) {
+void IotsaBLEDimmerMod::deviceFound(BLEAdvertisedDevice& device) {
   IFDEBUG IotsaSerial.printf("Found iotsaLedstrip/iotsaDimmer %s\n", device.getName().c_str());
   // Add the device, or update the connection information
   if (bleClientMod.addDevice(device.getName(), device)) {
   }
 }
 
-void iotsaBLEDimmerMod::loop() {
+void IotsaBLEDimmerMod::loop() {
+  // See if we have a value to save (because the user has been turning the dimmer)
+  if (saveAtMillis > 0 && millis() > saveAtMillis) {
+    saveAtMillis = 0;
+    configSave();
+  }
 
 }
 
 // Instantiate the Led module, and install it in the framework
-iotsaBLEDimmerMod bleDimmerControllerMod(application);
+IotsaBLEDimmerMod bleDimmerControllerMod(application);
 
 // Standard setup() method, hands off most work to the application framework
 void setup(void){
