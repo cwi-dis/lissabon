03d2b061f61af619a3851b1a655062caacd2611f
Jack Jansen <Jack.Jansen@cwi.nl>
2020-05-25 00:44:53 +0200

    Add 2901 and 2904 constants

--- src/LissabonBLE.cpp
+++ src/LissabonBLE.cpp
@@ -1,13 +1,44 @@
 #include "LissabonBLE.h"
+#include <BLE2904.h>
 
 namespace Lissabon {
 namespace Dimmer {
 // UUID of service advertised by iotsaLedstrip and iotsaDimmer devices
-BLEUUID serviceUUID("F3390001-F793-4D0C-91BB-C91EEB92A1A4");
-BLEUUID isOnUUID("F3390002-F793-4D0C-91BB-C91EEB92A1A4");
-BLEUUID identifyUUID("F3390003-F793-4D0C-91BB-C91EEB92A1A4");
-BLEUUID brightnessUUID("F3390004-F793-4D0C-91BB-C91EEB92A1A4");
-BLEUUID tempUUID("F3390005-F793-4D0C-91BB-C91EEB92A1A4");
-BLEUUID intervalUUID("F3390006-F793-4D0C-91BB-C91EEB92A1A4");
+const char* serviceUUIDstring = "F3390001-F793-4D0C-91BB-C91EEB92A1A4";
+BLEUUID serviceUUID(serviceUUIDstring);
+//const char* serviceUUID2901 = "Lissabon Dimmer Service";
+//const uint8_t serviceUUID2904format = ;
+//const uint16_t serviceUUID2904unit = ;
+
+const char* isOnUUIDstring = "F3390002-F793-4D0C-91BB-C91EEB92A1A4";
+BLEUUID isOnUUID(isOnUUIDstring);
+const char* isOnUUID2901 = "On/Off";
+const uint8_t isOnUUID2904format = BLE2904::FORMAT_UINT8;
+const uint16_t isOnUUID2904unit = 0x2700;
+
+const char* identifyUUIDstring = "F3390003-F793-4D0C-91BB-C91EEB92A1A4";
+BLEUUID identifyUUID(identifyUUIDstring);
+const char* identifyUUID2901 = "Request device identify";
+const uint8_t identifyUUID2904format = BLE2904::FORMAT_UINT8;
+const uint16_t identifyUUID2904unit = 0x2700;
+
+const char* brightnessUUIDstring = "F3390004-F793-4D0C-91BB-C91EEB92A1A4";
+BLEUUID brightnessUUID(brightnessUUIDstring);
+const char* brightnessUUID2901 = "Brightness";
+const uint8_t brightnessUUID2904format = BLE2904::FORMAT_UINT8;
+const uint16_t brightnessUUID2904unit = 0x27ad;
+
+const char* tempUUIDstring = "F3390005-F793-4D0C-91BB-C91EEB92A1A4";
+BLEUUID tempUUID(tempUUIDstring);
+const char* tempUUID2901 = "Color Temperature";
+const uint8_t tempUUID2904format = BLE2904::FORMAT_UINT16;
+const uint16_t tempUUID2904unit = 0x2700;
+
+const char* intervalUUIDstring = "F3390006-F793-4D0C-91BB-C91EEB92A1A4";
+BLEUUID intervalUUID(intervalUUIDstring);
+const char* intervalUUID2901 = "Lit pixel interval";
+const uint8_t intervalUUID2904format = BLE2904::FORMAT_UINT8;
+const uint16_t intervalUUID2904unit = 0x2700;
+
 };
 };
--- src/LissabonBLE.h
+++ src/LissabonBLE.h
@@ -7,11 +7,41 @@ namespace Lissabon {
 namespace Dimmer {
 // UUID of service advertised by iotsaLedstrip and iotsaDimmer devices
 extern BLEUUID serviceUUID;
+extern const char* serviceUUIDstring;
+//extern const char* serviceUUID2901;
+//extern const uint8_t serviceUUID2904format;
+//extern const uint16_t serviceUUID2904unit;
+
 extern BLEUUID isOnUUID;
+extern const char* isOnUUIDstring;
+extern const char* isOnUUID2901;
+extern const uint8_t isOnUUID2904format;
+extern const uint16_t isOnUUID2904unit;
+
 extern BLEUUID identifyUUID;
+extern const char* identifyUUIDstring;
+extern const char* identifyUUID2901;
+extern const uint8_t identifyUUID2904format;
+extern const uint16_t identifyUUID2904unit;
+
 extern BLEUUID brightnessUUID;
+extern const char* brightnessUUIDstring;
+extern const char* brightnessUUID2901;
+extern const uint8_t brightnessUUID2904format;
+extern const uint16_t brightnessUUID2904unit;
+
 extern BLEUUID tempUUID;
+extern const char* tempUUIDstring;
+extern const char* tempUUID2901;
+extern const uint8_t tempUUID2904format;
+extern const uint16_t tempUUID2904unit;
+
 extern BLEUUID intervalUUID;
+extern const char* intervalUUIDstring;
+extern const char* UUID2901;
+extern const uint8_t UUID2904format;
+extern const uint16_t UUID2904unit;
+
 };
 };
 #endif // _LISSABONBLE_H_
