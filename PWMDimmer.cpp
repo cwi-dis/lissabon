7948e7bfcc0f6e3f76c8827b8e0be66e7a1fc302
Jack Jansen <Jack.Jansen@cwi.nl>
2020-05-28 15:42:37 +0200

    Re-added code to actually operate PWM for the dimmer:-)

--- PWMDimmer.cpp
+++ PWMDimmer.cpp
@@ -57,7 +57,35 @@ void PWMDimmer::identify() {
 }
 
 void PWMDimmer::loop() {
+  // Quick return if we have nothing to do
+  if (millisAnimationStart == 0 || millisAnimationEnd == 0) {
+    // The dimmer shouldn't sleep if it is controlling the PWM output
+    if (level > 0 && isOn) iotsaConfig.postponeSleep(100);
+    return;
+  }
+  // Determine how far along the animation we are, and terminate the animation when done (or if it looks preposterous)
+  uint32_t thisDur = millisAnimationEnd - millisAnimationStart;
+  if (thisDur == 0) thisDur = 1;
+  float progress = float(millis() - millisAnimationStart) / float(thisDur);
+  float wantedLevel = level;
+  if (!isOn) wantedLevel = 0;
+  if (progress < 0) progress = 0;
+  if (progress >= 1) {
+    progress = 1;
+    millisAnimationStart = 0;
+    prevLevel = wantedLevel;
 
+    IFDEBUG IotsaSerial.printf("IotsaDimmer: wantedLevel=%f level=%f\n", wantedLevel, level);
+  }
+  float curLevel = wantedLevel*progress + prevLevel*(1-progress);
+  if (curLevel < 0) curLevel = 0;
+  if (curLevel > 1) curLevel = 1;
+  if (gamma && gamma != 1.0) curLevel = powf(curLevel, gamma);
+#ifdef ESP32
+  ledcWrite(channel, int(255*curLevel));
+#else
+  analogWrite(pin, int(255*curLevel));
+#endif
 }
 
 }
