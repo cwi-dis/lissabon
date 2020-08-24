5c3bf47a3ae93dee869b2748c18450fc7c99f640
Jack Jansen <Jack.Jansen@cwi.nl>
2020-08-25 00:21:13 +0200

    Postpone sleep for 2 minutes on ledstrip befure taking measurements

--- python/lissabonCalibrate/ledstrip.py
+++ python/lissabonCalibrate/ledstrip.py
@@ -6,9 +6,12 @@ class Ledstrip(Common):
     
     def post_open(self):
         self.service = self.device.ledstrip
+        # Disable automatic sleep for a few minutes
+        self.device.battery.set('postponeSleep', 120000)
         return True
 
     def close(self):
+        self.device.battery.set('postponeSleep', 0)
         pass
 
     def setColor(self, r=0, g=0, b=0, w=0):
