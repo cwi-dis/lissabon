73aee86d41369d15ce66d800b87e822103716f77
Jack Jansen <Jack.Jansen@cwi.nl>
2020-08-25 13:44:52 +0200

    Added parameters/values to csv. Added gamma correction parameter

--- python/lissabonCalibrate/__main__.py
+++ python/lissabonCalibrate/__main__.py
@@ -12,6 +12,8 @@ def main():
     parser.add_argument('--ledstrip', '-l', action='store', required=True, metavar='IP', help='Ledstrip hostname')
     parser.add_argument('--sensor', '-s', action='store', required=True, metavar='IP', help='Ledstrip sensor')
     parser.add_argument('--interval', action='store', type=int, metavar='DUR', help='Sensor integration duration (ms, between 40 and 1280)')
+    parser.add_argument('--w_gamma', action='store', type=float, metavar='GAMMA', help='Gamma value for W channel (default 1.0)')
+    parser.add_argument('--rgb_gamma', action='store', type=float, metavar='GAMMA', help='Gamma value for W channel (default 1.0)')
     parser.add_argument('--output', '-o', action='store', metavar='OUTPUT', help='CSV output filename')
     args = parser.parse_args()
 
@@ -26,30 +28,39 @@ def main():
     else:
         outputFile = sys.stdout
 
-    ofp = csv.DictWriter(outputFile, ['requested', 'w_lux', 'rgb_lux', 'rgbw_lux', 'w_cct', 'rgb_cct', 'rgbw_cct', 'lux'])
+    ofp = csv.DictWriter(outputFile, ['requested', 'w_lux', 'rgb_lux', 'rgbw_lux', 'w_cct', 'rgb_cct', 'rgbw_cct', 'parameter', 'value'])
     ofp.writeheader()
     for requested in VALUES:
         result = {'requested' : requested}
+        w_wanted = requested
+        rgb_wanted = requested
+        if args.w_gamma:
+            w_wanted = w_wanted ** args.w_gamma
+        if args.rgb_gamma:
+            rgb_wanted = rgb_wanted ** args.rgb_gamma
         # Do RGB-only color
-        lObj.setColor(r=requested, g=requested, b=requested)
+        lObj.setColor(r=rgb_wanted, g=rgb_wanted, b=rgb_wanted)
         time.sleep(1)
         sResult = sObj.get()
         result['rgb_lux'] = sResult['lux']
         result['rgb_cct'] = sResult['cct']
         # Do W-only color
-        lObj.setColor(w=requested)
+        lObj.setColor(w=w_wanted)
         time.sleep(1)
         sResult = sObj.get()
         result['w_lux'] = sResult['lux']
         result['w_cct'] = sResult['cct']
         # Do RGBW color
-        lObj.setColor(r=requested, g=requested, b=requested, w=requested)
+        lObj.setColor(r=rgb_wanted, g=rgb_wanted, b=rgb_wanted, w=w_wanted)
         time.sleep(1)
         sResult = sObj.get()
         result['rgbw_lux'] = sResult['lux']
         result['rgbw_cct'] = sResult['cct']
         ofp.writerow(result)
-
+    # Finally dump parameters and values
+    ofp.writerow(dict(parameter='interval', value=args.interval))
+    ofp.writerow(dict(parameter='w_gamma', value=args.w_gamma))
+    ofp.writerow(dict(parameter='rgb_gamma', value=args.rgb_gamma))
     outputFile.flush()
     if args.output:
         outputFile.close()
