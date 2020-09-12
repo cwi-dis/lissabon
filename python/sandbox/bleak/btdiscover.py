bad79cd81bb745082b54fdbde60139412bde1d16
Jack Jansen <Jack.Jansen@cwi.nl>
2020-09-12 22:53:28 +0200

    Updated and added inspect test program

---
+++ python/sandbox/bleak/btdiscover.py
@@ -0,0 +1,70 @@
+import asyncio
+import bleak
+
+VERBOSE=True
+FILTER=True
+
+async def discover_devices(duration=5):
+    devices = await bleak.discover(duration)
+    return devices
+    
+async def filter_devices(devices):
+    print(f'{len(devices)} devices:')
+    def has_name(d): return d.name and d.name != 'Unknown'
+    def has_battery(d): return 'battery' in d.metadata.get('uuids', [])
+    #devices = filter(lambda d:has_name(d) and has_battery(d), devices)
+    devices = filter(lambda d: has_name(d), devices)
+    devices = list(devices)
+    return devices
+    
+async def get_device_services(device):
+    error = None
+    services = None
+    try:
+        async with bleak.BleakClient(device.address) as server:
+            services = await server.get_services()
+    except bleak.exc.BleakError as e:
+        error = str(e)
+    return device, services, error
+
+def _unused():
+    print(f'\t{len(services)} services:')
+    for service in services:
+        print(f'\t\t{service}:')
+        for characteristic in service.characteristics:
+            print(f'\t\t\t{characteristic}')
+
+async def main():
+    devices = await discover_devices(10)
+    if VERBOSE:
+        print(f'{len(devices)} devices:')
+        for d in devices:
+            print(f'\tname={d.name}, address={d.address}')
+
+    if FILTER:
+        devices = await filter_devices(devices)
+        if VERBOSE:
+            print(f'{len(devices)} filtered devices:')
+            for d in devices:
+                print(f'\tname={d.name}, address={d.address}')
+
+    tasks = []
+    for device in devices:
+        tasks.append(asyncio.create_task(get_device_services(device)))
+    if tasks:
+        results = await asyncio.gather(*tasks)
+    else:
+        results = []
+    
+    for device, services, error in results:
+        print(f'Device {device}')
+        if services:
+            for service in services:
+                print(f'\tService {services}')
+        elif error:
+            print(f'\t{error}')
+        else:
+            print(f'\tunknown')
+    print('all done')
+    
+asyncio.run(main())
