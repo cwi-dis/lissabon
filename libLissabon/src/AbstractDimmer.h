df91ee733a0c99a601c74d56b102c40695c615ae
Jack Jansen <Jack.Jansen@cwi.nl>
2022-07-14 23:25:42 +0200

    BLE dimmers are starting to work. Problem was preposterous minLevel value

--- libLissabon/src/AbstractDimmer.h
+++ libLissabon/src/AbstractDimmer.h
@@ -50,28 +50,28 @@ public:
 public:
   int num;
   DimmerCallbacks *callbacks;
-  bool isOn;        // if true we want to show level, if false we want to be off.
+  bool isOn = 0;        // if true we want to show level, if false we want to be off.
 #ifdef DIMMER_WITH_LEVEL
-  float level;      // Requested light level
+  float level = 0;      // Requested light level
   float overrideLevel = -1; // If > 0 this is the _actual_ level we want to output, because a LED curve will cause it to be perceived as level.
-  float curLevel;   // actual current light level (depends on level/isOn but also animation progress)
-  float minLevel;   // minimum light level through UI
+  float curLevel = 0;   // actual current light level (depends on level/isOn but also animation progress)
+  float minLevel = 0;   // minimum light level through UI
 #endif
 #ifdef DIMMER_WITH_GAMMA
-  float gamma;
+  float gamma = 1;
 #endif // DIMMER_WITH_GAMMA
 #ifdef DIMMER_WITH_ANIMATION
-  float prevLevel;  // actual light level at millisAnimtationStart
-  int animationDurationMillis;  // Maximum duration of an animation (0-100% or reverse)
-  uint32_t millisAnimationStart;  // Time current animation started
-  uint32_t millisAnimationEnd;  // Time current animation should end
+  float prevLevel = 0;  // actual light level at millisAnimtationStart
+  int animationDurationMillis = 0;  // Maximum duration of an animation (0-100% or reverse)
+  uint32_t millisAnimationStart = 0;  // Time current animation started
+  uint32_t millisAnimationEnd = 0;  // Time current animation should end
 
 #endif // DIMMER_WITH_ANIMATION
 #ifdef DIMMER_WITH_TEMPERATURE
-  float temperature;
+  float temperature = 4000;
 #endif // DIMMER_WITH_TEMPERATURE
 #ifdef DIMMER_WITH_PWMFREQUENCY
-  float pwmFrequency;
+  float pwmFrequency = 50;
 #endif // DIMMER_WITH_PWMFREQUENCY
 protected:
   String name;
