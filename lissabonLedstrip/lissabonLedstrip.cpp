1ac1a9dded23540fac2e218f27588fbcc2892caf
Jack Jansen <Jack.Jansen@cwi.nl>
2023-05-29 01:03:26 +0200

    dimmerAvailableChanged signature fix

--- lissabonLedstrip/lissabonLedstrip.cpp
+++ lissabonLedstrip/lissabonLedstrip.cpp
@@ -101,7 +101,7 @@ public:
 protected:
   void dimmerOnOffChanged() override;
   void dimmerValueChanged() override;
-  void dimmerAvailableChanged(bool available, bool connected) override {};
+  void dimmerAvailableChanged() override {};
   bool getHandler(const char *path, JsonObject& reply) override;
   bool putHandler(const char *path, const JsonVariant& request, JsonObject& reply) override;
 private:
