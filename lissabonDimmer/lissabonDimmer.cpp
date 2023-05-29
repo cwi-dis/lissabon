7665fd3d8ef2d1c2d411d5c08df189e95b1a9420
Jack Jansen <Jack.Jansen@cwi.nl>
2023-05-29 23:20:49 +0200

    dimmerAvailableChanged signature changed.

--- lissabonDimmer/lissabonDimmer.cpp
+++ lissabonDimmer/lissabonDimmer.cpp
@@ -117,7 +117,7 @@ public:
 protected:
   void dimmerOnOffChanged() override;
   void dimmerValueChanged() override;
-  void dimmerAvailableChanged(bool available, bool connected) override {};
+  void dimmerAvailableChanged() override {};
   void handler();
   bool getHandler(const char *path, JsonObject& reply);
   bool putHandler(const char *path, const JsonVariant& request, JsonObject& reply);
