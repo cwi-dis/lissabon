8f92c614a66b407d4dcadc59e145a2a996d86bbe
Jack Jansen <Jack.Jansen@cwi.nl>
2025-12-26 15:37:42 +0100

    Fixes so that dimmers without DIMMER_WITH_LEVEL (so switches, really) work as expected.

--- lissabonDimmer/PWMDimmer.cpp
+++ lissabonDimmer/PWMDimmer.cpp
@@ -10,8 +10,21 @@ PWMDimmer::PWMDimmer(int _num, int _pin, int _channel, DimmerCallbacks *_callbac
 
 }
 
+#ifdef DIMMER_WITHOUT_LEVEL
+void PWMDimmer::switchLevel(bool on) {
+  if (on) {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  } else {
+    pinMode(pin, INPUT);
+  }
+}
+#endif
 
 void PWMDimmer::setup() {
+#ifdef DIMMER_WITHOUT_LEVEL
+  switchLevel(false);
+#else
 #ifdef ESP32
 #ifndef newer
   pinMode(pin, OUTPUT);
@@ -23,6 +36,7 @@ void PWMDimmer::setup() {
 #else
   pinMode(pin, OUTPUT);
 #endif
+#endif
 }
 
 bool PWMDimmer::available() {
@@ -32,6 +46,16 @@ bool PWMDimmer::available() {
 
 void PWMDimmer::identify() {
   IotsaSerial.printf("xxxjack identify dimmer%d channel %d pin %d\n", num, channel, pin);
+#ifdef DIMMER_WITHOUT_LEVEL
+  switchLevel(true);
+  delay(100);
+  switchLevel(false);
+  delay(100);
+  switchLevel(true);
+  delay(100);
+  switchLevel(false);
+  delay(100);
+#else
 #ifdef ESP32
   ledcWrite(channel, 128);
   delay(100);
@@ -51,21 +75,32 @@ void PWMDimmer::identify() {
   analogWrite(pin, 0);
   delay(100);
 #endif
+#endif
   updateDimmer();
 }
 
 void PWMDimmer::loop() {
   // Quick return if we have nothing to do
+#ifdef DIMMER_WITHOUT_LEVEL
+  if (isOn) {
+    iotsaConfig.postponeSleep(100);
+  }
+#else
   if (animationStartMillis == 0 || animationEndMillis == 0) {
     // The dimmer shouldn't sleep if it is controlling the PWM output
     if (level > 0 && isOn) iotsaConfig.postponeSleep(100);
     return;
   }
+#endif
   calcCurLevel();
+#ifdef DIMMER_WITHOUT_LEVEL
+  switchLevel(isOn);
+#else
 #ifdef ESP32
   ledcWrite(channel, int(255*curLevel));
 #else
   analogWrite(pin, int(255*curLevel));
+#endif
 #endif
 }
 
