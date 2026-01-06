3dca7f3dedb9c3dba9bc593859709d7548fd9578
Jack Jansen <Jack.Jansen@cwi.nl>
2026-01-06 15:52:07 +0100

    Getting started on 220V switch (hacked Kruidvat/Tuya).

--- lissabonDimmer/defs_220switch.h
+++ lissabonDimmer/defs_220switch.h
@@ -1,7 +1,7 @@
 
 // Pin to which MOSFET is attached. Channel is only relevant for esp32.
 
-#define PIN_PWM_DIMMER 0
+#define PIN_PWM_DIMMER 8
 #define CHANNEL_PWM_DIMMER (-1)
 
 // Uses no user interface at all (only controlled via wifi or bluetooth)
